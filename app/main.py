c533e45735ef419ec46c9c350c0c0b37fd151eb8
Manvith <148960168+manvith2003@users.noreply.github.com>
2026-01-23 12:19:59 +0530

    Fix FastAPI request body using Pydantic model

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 
 app = FastAPI()
 
+# Load trained model
 model = joblib.load("model.pkl")
 
+# Define request schema
+class WineFeatures(BaseModel):
+    features: list[float]
+
 @app.post("/predict")
-def predict(features: list):
-    pred = model.predict([features])[0]
+def predict(data: WineFeatures):
+    pred = model.predict([data.features])[0]
 
     return {
-        "name": "Manvith M",
+        "name": "Manvith",
         "roll_no": "2022BCS0066",
         "wine_quality": int(round(pred))
     }
